2de018a078c08f46f2b90f9e25ed0fc17f0fe16a
Brian Grant and Stephanie Gurung <brian+stephanie@>
2016-09-13 10:38:16 -0700

    allow the admin to toggle a user's admin status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to :back
+    end
+  end
+
   private
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation, :admin)
     end
 end
