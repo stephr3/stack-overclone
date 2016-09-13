b7452d78d462d02eb168be99519bad40a9c0fb0e
Brian Grant and Stephanie Gurung <brian+stephanie@>
2016-09-13 10:46:30 -0700

    allow admin to delete a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to :back
+  end
+
   private
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, :admin)
