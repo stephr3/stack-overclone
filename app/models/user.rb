6344a116637b8a9397b5caad725a5ff1afe61683
Brian Grant and Stephanie Gurung <brian+stephanie@>
2016-09-13 14:51:39 -0700

    filter users by admin privilege

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,17 @@ class User < ActiveRecord::Base
 
   attr_accessor :password
   validates_confirmation_of :password
-  validates :email, :password, presence: true
-  validates :password, length: { minimum: 4 }
+  validates :email, :password, presence: true, on: :create
+  validates :password, length: { minimum: 4 }, on: :create
   before_save :encrypt_password
+
+  scope(:where_admin, -> do
+    where({:admin => true})
+  end)
+
+  scope(:where_not_admin, -> do
+    where({:admin => false})
+  end)
 
   def encrypt_password
     self.salt = BCrypt::Engine.generate_salt
