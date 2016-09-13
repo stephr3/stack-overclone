40bb94055270023710783c242b26675f4896e56a
Brian Grant and Stephanie Gurung <brian+stephanie@>
2016-09-13 10:19:34 -0700

    allow the admin to view a list of users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913160023) do
+ActiveRecord::Schema.define(version: 20160913170742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160913160023) do
     t.string   "email"
     t.string   "password_hash"
     t.string   "salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "admin",         default: false
   end
 
 end
